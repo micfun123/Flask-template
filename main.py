169e11619702c3346702e9504dd261d1e444815d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-21 12:03:47 +0000

    demo finnished

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+#404 error (page not founds)
+@app.errorhandler(404)
+def page_not_found(e):
+    return str(e)
+
+#redirect to a different site
+@app.route("/redirect")
+def redirect_to_google():
+    return redirect("https://www.google.com")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port=80)
